8f98532610395ac914a9e4450e25a33219c55368
Steven Topazio <sat@northpointprinting.com>
2020-09-01 14:38:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
     index += 1 
-    puts "#{index}"
+    puts "#{index}. #{name}"
 end
 
 def summon_captain_planet# code an argument here

a022b98dfe81d5738744974af4d5f48832cdba31
Steven Topazio <sat@northpointprinting.com>
2020-09-01 14:47:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def find_the_cheese(array)
   i = 0 
   cheese_types = ["cheddar", "gouda", "camembert"]
-  while i > cheese_types.length 
+  while i < cheese_types.length 
   return cheese_types[i] if array.include?(cheese_types[i])
   i += 1 
 end

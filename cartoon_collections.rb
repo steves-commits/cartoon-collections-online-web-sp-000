3d0beb830de772f07b2773937619cbd05e5f6cc4
Steven Topazio <sat@northpointprinting.com>
2020-09-01 14:43:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  if short_words.length 
 end
 
 def find_the_cheese# code an argument here
